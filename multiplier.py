45aa23b9111e343c2ca39d87a33dab5925a6df02
Arthur Viana <46693700+VianaArthur@users.noreply.github.com>
2022-01-16 12:25:03 -0300

    Create multiplier.py

---
+++ multiplier.py
@@ -0,0 +1,12 @@
+def multiplier(a: int, b: int) -> int:
+    """Takes in two numbers, returns their product.
+    Args:
+        a (int): First number.
+        b (int): Second number.
+    Returns:
+        int: The product of the numbers.
+    """
+    
+    prod = a * b
+    
+    return prod
